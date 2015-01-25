b63b41ae9770989b0b7a7fb4f7017b5e7d482490
christian calloway <ccalloway@cancapital.com>
2015-01-25 16:55:23 -0500

    added ksum

--- ksum.rb
+++ ksum.rb
@@ -1,5 +1,21 @@
 #!/usr/bin/env ruby
 
-def ksum( array, target )
-  
-end
+public class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+  HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+  int[] result = new int[2];
+ 
+  for (int i = 0; i < numbers.length; i++) {
+    if (map.containsKey(numbers[i])) {
+      int index = map.get(numbers[i]);
+      result[0] = index+1 ;
+      result[1] = i+1;
+      break;
+    } else {
+      map.put(target - numbers[i], i);
+    }
+  }
+ 
+  return result;
+    }
+}
